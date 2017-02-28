cbf626144bdbb0929a3cf3b12f07254453c8c60f
Barrur Rhozi <Barrur Rhozi>
2017-02-28 15:20:32 +0700

    Minggu Kedua

---
+++ Minggu_Kedua/Mahasiswa.java
@@ -0,0 +1,8 @@
+/*program Mahasiswa*/
+class Mahasiswa {
+	public static void main(String[] args){
+		System.out.println("Nama	: Barrur Rhozi");
+		System.out.println("NIM	: 10115310");
+		System.out.println("Alamat	: Bandung");
+	}
+}
